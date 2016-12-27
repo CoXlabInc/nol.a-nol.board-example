4458c962d5582f1998b4342cfbeb09337ce870b7
Jongsoo Jeong <jsjeong@coxlab.kr>
2016-12-27 11:22:29 +0900

    Added rounding.

--- PWMTest/main.cpp
+++ PWMTest/main.cpp
@@ -6,8 +6,8 @@ float in = 0;
 static void taskAnalogWrite(void *) {
   float out = sin(in) * 127.5 + 127.5;
 
-  printf("* Duty cycle: %u\n", (uint8_t) out);
-  analogWrite(D2, (uint8_t) out);
+  printf("* Duty cycle: %u.%02u\n", (uint8_t) out, (uint8_t) (out * 100) % 100);
+  analogWrite(D2, (uint8_t) round(out));
 
   in += 0.01;
   if (in >= 6.283)
